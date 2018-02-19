7649c204268404553224a4b7ee824858d9aff344
Manoel Lobo <manoel.lobo@gmail.com>
2018-02-19 15:40:44 -0300

    Scaffolding
    Settings, configs, folders, linters, resolvers, debuggers & dependencies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import 'config/ReactotronConfig';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Text
+        </Text>
+      </View>
+    );
+  }
+}
