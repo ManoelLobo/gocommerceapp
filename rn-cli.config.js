14e7269178699e6747701215da9ac68c01bcddde
Manoel Lobo <manoel.lobo@gmail.com>
2018-02-20 23:13:31 -0300

    RN Vector Icons

---
+++ rn-cli.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+const blacklist = require('metro/src/blacklist');
+
+module.exports = {
+  getBlacklistRE() {
+    return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/])
+  },
+};
