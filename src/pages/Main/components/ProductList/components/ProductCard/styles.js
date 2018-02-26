9fedb0d44daa6ad28f3d24b50901e79c3093ac82
Manoel Lobo <manoel.lobo@gmail.com>
2018-02-25 22:57:03 -0300

    General Card styling

--- src/pages/Main/components/ProductList/components/ProductCard/styles.js
+++ src/pages/Main/components/ProductList/components/ProductCard/styles.js
@@ -1,12 +1,9 @@
 import { StyleSheet } from 'react-native';
-import { colors, measures, typography } from 'styles';
+import { general, colors, measures, typography } from 'styles';
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: colors.white,
-    margin: measures.baseSpacing,
-    padding: measures.baseSpacing,
-    borderRadius: (measures.baseSpacing / 2),
+    ...general.card,
     width: (measures.screenWidth / 2) - (measures.baseSpacing * 2),
   },
 
--- src/styles/general.js
+++ src/styles/general.js
@@ -1,8 +1,16 @@
 import colors from './colors';
+import measures from './measures';
 
 export default {
   container: {
     flex: 1,
     backgroundColor: colors.background,
+  },
+
+  card: {
+    backgroundColor: colors.white,
+    margin: measures.baseSpacing,
+    padding: measures.baseSpacing,
+    borderRadius: (measures.baseSpacing / 2),
   },
 };
