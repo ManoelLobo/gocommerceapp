50ecb2f1c39723053bfedc75b929068e8f933ccb
Manoel Lobo <manoel.lobo@gmail.com>
2018-02-28 09:37:19 -0300

    Refactored Cart store

--- src/store/ducks/cart.js
+++ src/store/ducks/cart.js
@@ -3,6 +3,8 @@ import { createActions, createReducer } from 'reduxsauce';
 // Types & Creators
 const { Types, Creators } = createActions({
   cartAddProduct: ['productId'],
+  cartIncreaseProduct: ['productId'],
+  cartDecreaseProduct: ['productId'],
   cartRemoveProduct: ['productId'],
 });
 
@@ -14,7 +16,9 @@ const INITIAL_STATE = {};
 
 export const addProduct = (state, action) => {
   const cart = { ...state };
-  cart[action.productId] = cart[action.productId] + 1 || 1;
+  if (!cart[action.productId]) {
+    cart[action.productId] = 1;
+  }
 
   return cart;
 };
@@ -26,7 +30,25 @@ export const removeProduct = (state, action) => {
   return cart;
 };
 
+export const increaseProduct = (state, action) => {
+  const cart = { ...state };
+  cart[action.productId] = cart[action.productId] + 1 || 1;
+
+  return cart;
+};
+
+export const decreaseProduct = (state, action) => {
+  const cart = { ...state };
+  if (cart[action.productId] > 1) {
+    cart[action.productId] -= 1;
+  }
+
+  return cart;
+};
+
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.CART_ADD_PRODUCT]: addProduct,
   [Types.CART_REMOVE_PRODUCT]: removeProduct,
+  [Types.CART_INCREASE_PRODUCT]: increaseProduct,
+  [Types.CART_DECREASE_PRODUCT]: decreaseProduct,
 });
