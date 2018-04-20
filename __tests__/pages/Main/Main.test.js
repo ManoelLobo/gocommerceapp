35e33ff1198f0ea8d801341c062a11a4d14316d1
Manoel Lobo <manoel.lobo@gmail.com>
2018-04-20 08:27:50 -0300

    Main page + Product list component tests [WIP] 🚧

--- __tests__/pages/Main/Main.test.js
+++ __tests__/pages/Main/Main.test.js
@@ -1,8 +1,59 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import sinon from 'sinon';
 
 import Main from 'pages/Main';
+import Header from 'components/Header';
+import CategoryBar from 'pages/Main/components/CategoryBar';
+import ProductList from 'pages/Main/components/ProductList';
+
+import CategoriesActions from 'store/ducks/categories';
+import CategoryProductsActions from 'store/ducks/category-products';
+
+const mockStore = configureStore({});
+
+const initialState = {
+  categories: {
+    data: [],
+    active: 1,
+  },
+  categoryProducts: { data: [] },
+};
+const changedState = {
+  categories: {
+    data: [],
+    active: 2,
+  },
+  categoryProducts: { data: [] },
+};
 
 describe('Testing Main page', () => {
-  it.skip();
+  const store = mockStore(initialState);
+  const createWrapper = () => shallow(<Main />, { context: { store } });
+
+  it('renders as expected', () => {
+    const wrapper = createWrapper();
+
+    expect(wrapper.dive().find(Header)).toHaveLength(1);
+    expect(wrapper.dive().find(CategoryBar)).toHaveLength(1);
+    expect(wrapper.dive().find(ProductList)).toHaveLength(1);
+  });
+
+  it.skip('requests products when category is changed', () => {
+    const wrapper = createWrapper();
+    const changedStore = mockStore(changedState);
+    sinon.spy(CategoryProductsActions, 'categoryProductsRequest');
+
+    wrapper.setContext({ store: changedStore });
+    wrapper.setProps({ categories: { active: 2 } });
+
+    expect(CategoryProductsActions.categoryProductsRequest.called).toBe(true);
+
+    expect(store.getActions()).toContainEqual();
+  });
+
+  it.skip('data to be loaded on mount', () => {
+    const wrapper = createWrapper();
+  });
 });
--- __tests__/pages/Main/components/ProductList.test.js
+++ __tests__/pages/Main/components/ProductList.test.js
@@ -1,8 +1,83 @@
 import React from 'react';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
 import ProductList from 'pages/Main/components/ProductList';
 
+const mockStore = configureStore({});
+
+const initialState = {
+  categoryProducts: {
+    data: [
+      {
+        id: 1,
+        name: 'Camisa',
+        brand: 'Marca',
+        image: 'imguri',
+        price: 50,
+      },
+      {
+        id: 2,
+        name: 'Camiseta',
+        brand: 'Marca',
+        image: 'imguri',
+        price: 45.66,
+      },
+    ],
+    loading: false,
+  },
+};
+
+const loadingState = {
+  categoryProducts: {
+    data: [],
+    loading: true,
+  },
+};
+
+const emptyState = {
+  categoryProducts: {
+    data: [],
+    loading: false,
+  },
+};
+
 describe('Testing ProductList', () => {
-  it.skip();
+  const defaultStore = mockStore(initialState);
+  const createWrapper = (store = defaultStore) => shallow(<ProductList />, { context: { store } });
+  const createLoadingWrapper = () => createWrapper(mockStore(loadingState));
+  const createEmptyWrapper = () => createWrapper(mockStore(emptyState));
+
+  it('renders loaded as expected', () => {
+    const wrapper = createWrapper();
+
+    expect(wrapper
+      .dive()
+      .find(FlatList)
+      .exists()).toBe(true);
+    expect(wrapper
+      .dive()
+      .find(ActivityIndicator)
+      .exists()).toBe(false);
+  });
+
+  it('renders loading as expected', () => {
+    const wrapper = createLoadingWrapper();
+
+    expect(wrapper
+      .dive()
+      .find(ActivityIndicator)
+      .exists()).toBe(true);
+    expect(wrapper
+      .dive()
+      .find(FlatList)
+      .exists()).toBe(false);
+  });
+
+  it('renders empty as expected', () => {
+    const wrapper = createEmptyWrapper();
+
+    expect(wrapper.dive().contains(<Text>Não há produtos disponíveis nesta seção.</Text>)).toBe(true);
+  });
 });
